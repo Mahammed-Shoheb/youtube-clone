06d0f08804b3adf8bdcbfe255b2b3c466ec60ee6
Mahammed Shoheb <mdshoheb135@gmail.com>
2024-02-07 04:07:13 +0530

    fix context.jsx name to Context.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import { AppContext } from './utility/context.jsx';
+import { AppContext } from './utility/Context.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
